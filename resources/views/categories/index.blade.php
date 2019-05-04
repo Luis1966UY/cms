176c6d7ea9851400a9aca7b283b0a10a29350c29
Luis Carlos Silva <lcsm1966@gmail.com>
2019-05-04 16:43:27 -0300

    implement the permanent delete and the image file delete

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,48 +14,51 @@ CMS- Categories
             Categories
         </div>
         <div class="card-body">
-            <ul class="list-group">
-            @foreach($categories as $category)
+            @if($categories->count() > 0)
+                <ul class="list-group">
+                @foreach($categories as $category)
 
-                <li class="list-group-item">
-                    {{ $category->name }}
+                    <li class="list-group-item">
+                        {{ $category->name }}
 
-                    
-                    {{-- <a href="/categories/{{ $category->id }}/delete" class="btn btn-danger btn-sm float-right mx-2">Delete</a>  --}}
-                    <button class="btn btn-danger btn-sm float-right mx-2" onclick="handleDelete({{ $category->id }})">Delete</button>
-                    <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-success btn-sm float-right" >Edit</a> 
+                        
+                        {{-- <a href="/categories/{{ $category->id }}/delete" class="btn btn-danger btn-sm float-right mx-2">Delete</a>  --}}
+                        <button class="btn btn-danger btn-sm float-right mx-2" onclick="handleDelete({{ $category->id }})">Delete</button>
+                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-success btn-sm float-right" >Edit</a> 
 
-                </li>
+                    </li>
 
-            @endforeach
-            </ul>
-
-            <!-- Modal -->
-            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <form action="" method="POST" id="deleteCategoryForm">
-                        @csrf
-                        @method('DELETE')
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Delete category</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                    @endforeach
+                </ul>
+                <!-- Modal -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <form action="" method="POST" id="deleteCategoryForm">
+                            @csrf
+                            @method('DELETE')
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteModalLabel">Delete category</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="text-center text-bold">
+                                        Are you sure you want to delete this category?
+                                    </p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go back</button>
+                                    <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                <p class="text-center text-bold">
-                                    Are you sure you want to delete this category?
-                                </p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go back</button>
-                                <button type="submit" class="btn btn-danger">Yes, Delete</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            @else
+                <h3 class="text-center">No categories yet</h3>
+            @endif
 
         </div>
     </div>
